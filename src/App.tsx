08a634d93b4bf14a25eed60e9583b00b5f80f828
Pablo Thobias <pablo.carminatti@newsela.com>
2024-01-17 15:36:59 -0300

    feature(DataFetching): fetching posts using typescript and native fetch method

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from 'react';
+import { get } from './util/http.ts';
+import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
+import fetchingImg from './assets/data-fetching.png';
+
+type RawBlogPostData = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [posts, setPosts] = useState<Array<BlogPost>>([]);
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const data = await get('https://jsonplaceholder.typicode.com/posts') as Array<RawBlogPostData>;
+
+        if (data?.length) {
+
+          const formattedData: Array<BlogPost> = data.map((post) => ({
+            id: post.id,
+            title: post.title,
+            text: post.body,
+          }));
+
+          setPosts(formattedData);
+        }
+      };
+
+      fetchData();
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    }
+  }, []);
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="An abstract image depicting a data fetching process " />
+      {!!posts?.length && <BlogPosts posts={posts} />}
+      {!!error && <p>{error}</p>}
+    </main>
+  );
 }
 
 export default App;
